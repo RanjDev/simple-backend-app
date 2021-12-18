5a9b9705c2593fcc8d03bbe9e58b22715ac46027
Ranj Jalal <ranj.jalal97@gmail.com>
2021-12-18 17:47:13 +0300

    Authorization problem solved

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config("dotenv");
 // middleware to check if the user is logged in
 export function isLoggedIn(req, res, next) {
   // const token = req.headers.authorization.replace("Bearer ", "");
@@ -22,7 +24,11 @@ export function isLoggedIn(req, res, next) {
 //middleware to check if the user is admin and has admin privileges
 export function isAdmin(req, res, next) {
   let token = req.headers.authorization;
+  if (token === undefined || token === "") {
+    return res.status(400).json({ error: "Not Authenticated" });
+  }
 
+  token = token.replace(/^Bearer\s+/, "");
   try {
     const user = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
     if (user.role === "admin") {
